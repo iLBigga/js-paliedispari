c9b014a4aca118fff05f8b553c0f7076ceb69a78
iLBigga <Giannanti1996@gmail.com>
2022-07-29 15:40:39 +0200

    Esercizio Pari e Dispari completato

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 //Chiedo all'utente di inserire una parola
-let userWorld = prompt('Inseriwsci una parola');
-while (userWorld  == '') {
-    userWorld = prompt('Inseriwsci una parola');
-}
+let userWorld 
+do {
+    userWorld = prompt('Inseriwsci una parola').toLowerCase();
+}while (userWorld  == '')
 
-// Divio le lettere 
-const letterSplit = userWorld.split('')
-// console.log(letterSplit)
 
 // Creo funzione per vedere userWorld è palindroma
 function palindrmo (elenco) {
 
+    // Divio le lettere 
+    const letterSplit = elenco.split('');
     // Mi salvo la lunghezza dell'array selezionato
-    let arrayLength = elenco.length;
+    let arrayLength = letterSplit.length;
     // Creo variabile per salvarmi la posizione
     let arrayPosition;
     // Creo nuovo array 
@@ -59,6 +58,46 @@ function palindrmo (elenco) {
     return result;
 }
 
-console.log(palindrmo(letterSplit));
+console.log(palindrmo(userWorld))
 
+// Utilizzo queryseceltion per selezionare il tasto di invio
+const submitElement = document.querySelector('input[type="submit"]');
 
+// Creo funzione per creare numero random
+function randomNum(max, min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
+}
+// Creo variabile per verificare se il numero è pari
+const isEven = (num) => num % 2 === 0 ;
+// Creo variabile per verificare se il numero è dispari
+const isOdd = (num) => num % 2 === 1 ;
+
+// Creo variabile per selezionare il numero inserito e se l'utente sceglie pari o dispari
+let userNumber, userChoise;
+
+// utilizzo eventlistener per ricever ei dati inseriti dall'utente
+submitElement.addEventListener('click', function () {
+    //Ricevo dati del numero inserio
+    userNumber = document.querySelector('input[type="text"');
+    userNumber = parseInt(userNumber.value);
+    // Ricevo dati della scelta pari o dispari
+    userChoise = document.querySelector('select');
+    userChoise = userChoise.value; 
+    console.log('Hai selezionato', userChoise)
+
+    // Creo costante con fuzione RandomNum
+    const botNumber = randomNum(1,5);
+    // Calcolo somma di user e di bot number
+    const somma = userNumber + botNumber;
+    //SE l'utente ha selezionato pari ed il numero e pari O se l'utente ha selezionato dispari ed il risultato è dispari
+    if ((userChoise === 'pari' && isEven(somma)) || (userChoise === 'dispari' && isOdd(somma))) {
+        // Stampo Hai vito
+        console.log('Hai vinto', userNumber, '+', botNumber, '=', somma);
+    // ALTRIMENTI
+    } else {
+        // Stampo hai perso
+        console.log('Hai perso',userNumber, '+', botNumber, '=', somma);
+    } 
+})
